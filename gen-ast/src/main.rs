ca65eb305ede70495684938c8acf34236ccfac62
caozongying <>
2022-09-19 21:02:04 +0800

    能够获取参数
    Signed-off-by: caozongying <>

--- gen-ast/src/main.rs
+++ gen-ast/src/main.rs
@@ -1,4 +1,22 @@
+extern crate getopts;
+use getopts::Options;
+use std::env;
 
+fn build_args(){
+
+    let args: Vec<String> = env::args().collect();
+
+    let mut opts = Options::new();
+
+    opts.optopt("i", "--input", "需要解析的文件名称", "input");
+
+    let matches = match opts.parse(&args[1..]){
+        Ok(m)=>{m}
+        Err(e)=>{panic!("{}", e)}
+    };
+
+    println!("{}", matches.opt_str("i").unwrap());
+}
 
 fn main() {
     use std::io::stderr;
@@ -11,6 +29,8 @@ fn main() {
     use swc::ecmascript::ast::{ModuleItem};
     use swc_atoms::{JsWord};
     use swc_ecma_ast::{ImportSpecifier, Str};
+
+    build_args();
 
     let source = "
     import React from \"react\";
@@ -82,51 +102,6 @@ fn main() {
     println!("parser success");
 
     let s = serde_json::to_string_pretty(&module).expect("failed to serialize");
-    println!("ast json is \n {}", s);
-
-
-    let cm: Lrc<SourceMap> = Default::default();
-    let fm = cm.new_source_file(
-      FileName::Custom("test.js".into()),
-      source.into(),
-    );
-
-    let compiler = Compiler::new(cm.clone());
-    // let _handler = Handler::with_emitter_writer(Box::new(stderr()), Some(compiler.cm.clone()));
-
-    let lexer = Lexer::new(
-      // We want to parse ecmascript
-      Syntax::Typescript(TsConfig {
-        tsx: true,
-        decorators: true,
-        dynamic_import: true,
-        dts: false,
-        no_early_errors: false,
-        import_assertions: false,
-      }),
-      // EsVersion defaults to es5
-      EsVersion::Es2016,
-      StringInput::from(&*fm),
-      None,
-    );
-
-    let mut parser = Parser::new_from(lexer);
-
-    let list_error = parser.take_errors();
-    if list_error.iter().len() > 0 {
-      let mut err_msg = "".to_owned();
-      for err in list_error {
-        let msg = err.into_kind().msg().to_string();
-        err_msg.push_str(msg.as_str());
-      }
-    }
-
-    let mut module = parser
-      .parse_module().unwrap();
-
-    println!("parser success");
-
-    let s = serde_json::to_string_pretty(&module).expect("failed to serialize");
-    println!("ast json is \n {}", s);
+    // println!("ast json is \n {}", s);
 
 }
